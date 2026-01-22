13535ec2d558444e1a630383719e6e4177f464c3
Blackbird-1509 <sohammisra1509@gmail.com>
2026-01-22 15:08:16 +0530

    Added team listing API
    Dynamic route which accepts the teamId in the fetch api and filters
    likewise
    Provides details as shown on UI page

---
+++ src/app/api/teams/[teamId]/route.js
@@ -0,0 +1,39 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET( { params }) {
+  const { teamId } = params;
+
+  if (!teamId) {
+    return NextResponse.json({ error: "teamId is required" }, { status: 400 });
+  }
+
+  try {
+    const team = await prisma.team.findUnique({
+      where: { id: teamId },
+      select: {
+        name: true,
+        track: true,
+        vitStudents: {
+          select: {
+            name: true,
+            regNo: true,
+          },
+        },
+        figmaLink: true,
+        githubLink: true,
+        pptLink: true,
+        otherLinks: true
+      },
+    });
+
+    if (!team) {
+      return NextResponse.json({ error: "Team not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(team);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
