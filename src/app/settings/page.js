516e265f1b9304346e365beca2e0d7af299528ce
Blackbird-1509 <sohammisra1509@gmail.com>
2026-02-04 00:16:25 +0530

    Finished teamdetails UI
    UI finished, untested due to lack of seeded data

--- src/app/settings/page.js
+++ src/app/settings/page.js
@@ -26,7 +26,7 @@ export default function Settings() {
   return (
     <div className="relative min-h-screen bg-black/50">
       <div className="absolute inset-0 rounded-[1280px] bg-[#669C7D] blur-[375px] opacity-20"></div>
-      <p className="text-white p-8 text-[20px] font-bold">SETTINGS</p>
+      <h1 className="text-white p-8 text-[20px] font-bold">SETTINGS</h1>
       <div className="flex justify-center">
       <form onSubmit={handleSubmit} className="mt-20 w-150.5 h-auto flex flex-col justify-center rounded-[25px] bg-[rgba(255,255,255,0.10)] backdrop-blur-md border border-white/30 inset-shadow-sm/30 shadow-xl p-8 gap-6">
       <h1 className="text-[24px] pt-6">ADD NEW ADMIN</h1>
