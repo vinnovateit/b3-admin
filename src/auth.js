aa8a02db8a55965a26180f077f49af80a103a798
TechPro424 <86620085+TechPro424@users.noreply.github.com>
2026-02-05 00:28:19 +0530

    fix: use prisma client from lib folder

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,8 @@
 
 import NextAuth from "next-auth"
 import authConfig from "./auth.config";
-import { PrismaClient }  from "@prisma/client"
+import prisma from "@/lib/prisma";
 
-const prisma = new PrismaClient();
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
